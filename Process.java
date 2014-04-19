dba705ec0f8a33411dafd24cd0e01b4192765814
zaynashahzad <zaynashahzad24@gmail.com>
2014-04-19 11:01:50 -0700

    incomplete vs run!

--- Process.java
+++ Process.java
@@ -2,14 +2,13 @@ import java.util.Random;
 
 public class Process {
 
-
-
     private int id;
     private int timeToRun = 0;
     private int numCollisions; // this is n
     private boolean collisionFound = false;
     private int totalCount = 0;
-    private Random r; 
+    private boolean isDone = false;
+    private Random r;
 
     public Process(int id) {
         calculateTimeToRun();
@@ -17,20 +16,26 @@ public class Process {
         r = new Random();
     }
 
-    
-    
-    public boolean isComplete() {
-
-        if (collisionFound) {
-            numCollisions++;
-            calculateTimeToRun();
-            return false;
-        }
+    public boolean canRun() {
         return timeToRun == 0;
 
     }
 
+    public void runIt() {
+        if (collisionFound) {
+            numCollisions++;
+            calculateTimeToRun();
+        } else {
+
+            decTime();
+        }
+
+    }
+
     public void decTime() {
+        if (timeToRun == 0) {
+            setDone(true);
+        }
 
         if (timeToRun > 0) {
             timeToRun--;
@@ -48,42 +53,29 @@ public class Process {
 
     }
 
-    
     public int getId() {
         return id;
     }
-
-
 
     public void setId(int id) {
         this.id = id;
     }
 
-
-
     public int getTimeToRun() {
         return timeToRun;
     }
-
-
 
     public void setTimeToRun(int timeToRun) {
         this.timeToRun = timeToRun;
     }
 
-
-
     public int getNumCollisions() {
         return numCollisions;
     }
 
-
-
     public void setNumCollisions(int numCollisions) {
         this.numCollisions = numCollisions;
     }
-
-
 
     public boolean isCollisionFound() {
         return collisionFound;
@@ -96,5 +88,13 @@ public class Process {
     public void setTotalCount(int totalCount) {
         this.totalCount = totalCount;
     }
-    
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
 }
